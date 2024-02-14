182c252e7452c2c7af4a459d2cf29d01901f23d5
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-14 11:01:26 +0200

    Create inventory modle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_124331) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_085528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_124331) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +49,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_124331) do
     t.index ["unconfirmed_email"], name: "index_users_on_unconfirmed_email"
   end
 
+  add_foreign_key "inventories", "users"
 end
