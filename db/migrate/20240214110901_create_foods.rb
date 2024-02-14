f4f713178d9198aa8a7189c92e15d9e2fb0c9766
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2024-02-14 12:56:55 +0100

    Fix linters

--- db/migrate/20240214110901_create_foods.rb
+++ db/migrate/20240214110901_create_foods.rb
@@ -1,6 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.1]
   def change
+    drop_table :foods if table_exists?(:foods)
+
     create_table :foods do |t|
+      t.string :name
+      t.string :measurement_unit, null: false
+      t.decimal :price_dollars, null: false, default: 0.0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_124331) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_111523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
-    t.string "measurement_unit"
-    t.decimal "price"
+    t.string "measurement_unit", null: false
+    t.decimal "price_dollars", default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
