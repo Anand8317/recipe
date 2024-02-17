b301d76a7b5e58cf02936be5aa402a7807211af4
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-18 00:26:41 +0200

    Fix linter errors

--- spec/models/culinary_recipe_spec.rb
+++ spec/models/culinary_recipe_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.describe CulinaryRecipe, type: :model do
   it 'has a valid factory' do
-    CulinaryRecipe = FactoryBot.build(:CulinaryRecipe)
-    expect(CulinaryRecipe).to be_valid
+    culinary_recipe = FactoryBot.build(:culinary_recipe)
+    expect(culinary_recipe).to be_valid
   end
 
   it 'is not valid without a name' do
-    CulinaryRecipe = FactoryBot.build(:CulinaryRecipe, name: nil)
-    expect(CulinaryRecipe).to_not be_valid
+    culinary_recipe = FactoryBot.build(:culinary_recipe, name: nil)
+    expect(culinary_recipe).to_not be_valid
   end
 
   it 'belongs to a user' do
-    user_association = CulinaryRecipe.reflect_on_association(:user)
+    user_association = culinary_recipe.reflect_on_association(:user)
     expect(user_association.macro).to eq(:belongs_to)
   end
 
-  it 'has many CulinaryRecipe_foods' do
-    CulinaryRecipe = CulinaryRecipe.reflect_on_association(:CulinaryRecipe_foods)
-    expect(CulinaryRecipe.macro).to eq(:has_many)
+  it 'has many culinary_recipe_foods' do
+    culinary_recipe = culinary_recipe.reflect_on_association(:culinary_recipe_foods)
+    expect(culinary_recipe.macro).to eq(:has_many)
   end
 end
