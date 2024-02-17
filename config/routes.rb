a37f5a23fa4b0154d0401a8a8499b488f7ca1e5c
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-17 18:42:12 +0200

    Add navigation and public recipe

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,15 @@ Rails.application.routes.draw do
   end
 
   resources :inventories, only: [:index, :show, :new, :create, :destroy]
+  resources :foods
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get '/public_recipes', to:'culinary_recipes#public_recipes', as: :public_recipes
+
 
   # Defines the root path route ("/")
   root "culinary_recipes#index"
